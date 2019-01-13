eb56f40a8a4da53c3d1507ffc13bc04134dbc231
andreahowes <ashcreek.howes@gmail.com>
2019-01-13 13:35:29 -0500

    changed Dijkstras shortest path to be object oriented

--- src/main/java/DijkstrasShortestPath.java
+++ src/main/java/DijkstrasShortestPath.java
@@ -1,9 +1,47 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class DijkstrasShortestPath {
     public static void main(String[] args) {
+        //creating graph:
+        Vertex vertexA = new Vertex("A");
+        Vertex vertexB = new Vertex("B");
+        Vertex vertexC = new Vertex("C");
+        Vertex vertexD = new Vertex("D");
+        vertexA.addNeighbor(vertexB, 10);
+        vertexA.addNeighbor(vertexC, 20);
+        vertexB.addNeighbor(vertexC, 5);
+        vertexB.addNeighbor(vertexD, 16);
+        vertexC.addNeighbor(vertexD, 20);
+        Graph graph = new Graph();
+        graph.addVertex(vertexA);
+        graph.addVertex(vertexB);
+        graph.addVertex(vertexC);
+        graph.addVertex(vertexD);
+        //find shortest path:
+        //method needs to take in graph and the source vertex
+        //set the distance of the source vertex as 0
+        //create an empty HashSet of vertices with perminent distances
+        //create HashSet of vertices with NonPerminent distances and add source vertex
+        //while the Hashset of NonPerminent distances is greater than 0:
+            //set currentVertexAlgorithmIsOn to the vertex with shortest distance in NonPerminentDistances
+            //then remove that from the NonPerminent List
+            //then cycle through all neighboringVertices and:
+                // get distance from current vertex
+                // if the neightbor vertex isn't on the PerminentDistance list:
+                    //add to the NonPerminentDistance List
 
 
-        //algorithm to find the shortest path between all vertices
-        //works by finding all paths and picking the shortest one
+
+
+
+
+    }
+}
+//algorithm to find the shortest path between all vertices
+//works by finding all paths and picking the shortest one
 //
 //          10--b--16
 //          |   |   |
@@ -11,48 +49,70 @@ public class DijkstrasShortestPath {
 //          |   |   |
 //          20--c--20
 
-        //graph for distances in the above example
-        int myGraph[][] = new int[][]{{0, 10, 20, 0}, {10, 0, 5, 16}, {20, 5, 0, 20}, {0, 16, 20, 0}};
-        int numberOfVertices = myGraph[0].length;
+//class to create the graph
+class Graph {
+    //graph contains a HashSet that will contain all vertices.
+    private Set<Vertex> vertices = new HashSet<>();
 
-        //starting the shortest path search at (a)
-        int start = 0;
-
-
-        // this int array will contain the shortest distance from start to current vertex
-        int minDistance[] = new int[numberOfVertices];
-
-        //starting the minDistance for each vertice as infinite
-        for (int i = 0; i < numberOfVertices; i++) {
-            minDistance[i] = Integer.MAX_VALUE;
-        }
-        //setting min distance for starting point to 0
-        minDistance[start]= 0;
-
-        //this boolean array will contain true when the path to that vertex has been finalized
-        boolean setDistance[] = new boolean[numberOfVertices];
-
-        //starting the setDistance for each vertex as false
-        for (int i = 0; i < numberOfVertices; i++) {
-            setDistance[i] = false;
-        }
-
-        //finding shortest path method
-        //1. pick the min distance vertex from the set of vertices not yet processed- always equal to starting vertex in first iteration
-            // -method should have parameters of minDistance[] and setDistance[]
-            // -initialize infinite distance for all vertices here?
-            // - loop through all setDistance[]
-                    // -if value is false (min distance not set)
-                    // - and distance is less than current min
-                    // - set minimum as that distance and the min vertex as the current vertex
-        //2. mark the picked vertex as processed (true)
-        //3. update the distance value for the adjacent vertices of the picked vertex
-            // -must be false in setDistance (shortest path not yet determined)
-            // -must be next to current vertex
-            // -must have path through current vertex be smaller than current value of distance
-
-
+    //method to add vertex
+    public void addVertex(Vertex vertex) {
+        vertices.add(vertex);
     }
 
+    //getters and setters needed?
+    public Set<Vertex> getVertices() {
+        return vertices;
+    }
 
+    public void setVertices(Set<Vertex> vertices) {
+        this.vertices = vertices;
+    }
 }
+
+// vertex for each point on the graph
+class Vertex {
+    //each vertex needs a name
+    private String name;
+    //vertex needs a int to keep track of distance- automatically set to Max Int first
+    private Integer distance;
+    //each vertex needs a hashmap of neighboring vertices and their distances
+    private Map<Vertex, Integer> neighboringVertices = new HashMap<>();
+    //method to add neighboring vertex and distance to hashMap
+    public void addNeighbor(Vertex vertex, Integer distance){
+        neighboringVertices.put(vertex, distance);
+    }
+
+    public Map<Vertex, Integer> getNeighboringVertices() {
+        return neighboringVertices;
+    }
+
+    public void setNeighboringVertices(Map<Vertex, Integer> neighboringVertices) {
+        this.neighboringVertices = neighboringVertices;
+    }
+
+    //constructor
+    public Vertex(String name) {
+        this.name = name;
+    }
+
+    //getters, setters needed?
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+}
+
+
+
+
